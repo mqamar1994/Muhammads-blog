a57e89583f20a5f0c2980016019c6e1417764ad5
Muhammad Qamaruddin <mqamar1994@gmail.com>
2016-01-30 11:26:59 +0000

    Add Edit,Show and index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,29 +1,44 @@
 class ArticlesController < ApplicationController
-
-def new
-        @article = Article.new
-end
-
-def create
-    # render plain: params[:article].inspect
-    # For displaying values passed to controller
-    @article = Article.new(article_params)
-    if @article.save
-        flash[:notice] = "Article was successfully created"
-        redirect_to article_path(@article)
-    else
-        render 'new'
+    def index
+        @articles = Article.all
+    end
+    def new
+            @article = Article.new
     end
     
-end
-
-def show
-   @article = Article.find(params[:id])
-end
-
-private 
-def article_params
-    params.require(:article).permit(:title, :description)
-end
-
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
+    def create
+        # render plain: params[:article].inspect
+        # For displaying values passed to controller
+        @article = Article.new(article_params)
+        if @article.save
+            flash[:notice] = "Article was successfully created"
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was successfully edited"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+    end
+    
+    def show
+       @article = Article.find(params[:id])
+    end
+    
+    private 
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
+    
 end
