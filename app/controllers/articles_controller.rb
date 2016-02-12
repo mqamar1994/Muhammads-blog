e6c15d21371a65297fe874433885fe21d38331b3
Muhammad Qamaruddin <mqamar1994@gmail.com>
2016-02-12 20:16:35 +0000

    Add associations between user and articles and add debugger view for dev env

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,7 @@ class ArticlesController < ApplicationController
         # render plain: params[:article].inspect
         # For displaying values passed to controller
         @article = Article.new(article_params)
+        @article.user = User.first
         if @article.save
             flash[:success] = "Article was successfully created"
             redirect_to article_path(@article)
