0a280a25b5e80ece34549062898ebf2469991daf
Muhammad Qamaruddin <mqamar1994@gmail.com>
2016-02-12 21:40:28 +0000

    Added link to signup from button and navbar from homepage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-          flash[:success] = "Welcome to Muhammad's blog #{@user.username}"
+          flash[:success] = "Welcome to Muhammad's blog #{@user.username}!"
           redirect_to articles_path
         else
             render 'new'
